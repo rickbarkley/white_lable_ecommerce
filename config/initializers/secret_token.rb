bc61acb93e020b80c52fabc053eeebd53eb94576
rick_barkley <rick_barkley@nuvola-networks.com>
2013-11-10 21:00:55 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WlEcommerse::Application.config.secret_token = '1d621e8b181ca98581292924025bf05057aa3cf85bc3c89c130fdc0f9c6810c0f1be504157259a9edc63d6147010efbd7f9180d1cc9717b6a608d1deb72e1f1b'
